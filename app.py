a1ede5b9a131aa43bb17994f0e62110a6b4fe5e1
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-08-29 10:19:37 -0400

    Add app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine(
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
