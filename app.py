aa972ce417fc754966688efaacb7e6bc52b27689
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-09-01 15:56:19 -0400

    Folder cleanup

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask
+from flask import jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine(
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -19,10 +21,13 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-Passenger = Base.classes.passenger
+# We can view all of the classes that automap found
+Base.classes.keys()
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
+
+app.run(debug=True)
