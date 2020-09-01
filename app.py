66e3eb226e31ca497acf1511157b8aa21f507f0e
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-09-01 16:54:49 -0400

    setup home route

--- app.py
+++ app.py
@@ -9,25 +9,28 @@ from sqlalchemy import create_engine, func
 from flask import Flask
 from flask import jsonify
 
-
-#################################################
 # Database Setup
-#################################################
 engine = create_engine("sqlite:///hawaii.sqlite")
-
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-
-# Save reference to the table
 # We can view all of the classes that automap found
 Base.classes.keys()
+#Measurement = Base.classes.measurement
+#Station = Base.classes.station
 
-#################################################
 # Flask Setup
-#################################################
+#Create an app, being sure to pass __name__
 app = Flask(__name__)
 
+#Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my climate API for Hawaii. Surf's Up!"
 
-app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
